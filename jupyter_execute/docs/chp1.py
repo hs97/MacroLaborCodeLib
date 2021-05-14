d49548f293df82f899b61aa7fb8c9b867fadad3e
hs97 <haoyusheng97@gmail.com>
2021-05-14 11:49:08 -0700

    Update documentation

---
+++ jupyter_execute/docs/chp1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # The Importance of Unemployment
+# 
+# ## 1. Introduction
+# 
+# This text examines unemployment. We begin by trying to motivate why we want to study unemployment, why unemployment is such an important problem in macroeconomics. Then, we will introduce the matching function which will be a key tool we use to study unemployment. I will show you some data to motivate the use of the matching function and then we will study the theoretical properties of the matching function.
+# 
+
+# In[ ]:
+
+
+print("Hello World")
+
